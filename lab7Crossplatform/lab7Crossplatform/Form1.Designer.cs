154397f122d07e2f738d880b2630a4c377324d8c
Anache-jk <83112485+Anache-jk@users.noreply.github.com>
2022-05-25 16:16:59 +0300

    Add files via upload

---
+++ lab7Crossplatform/lab7Crossplatform/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace lab7Crossplatform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(26, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(26, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(44, 81);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(26, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(94, 81);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(26, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(44, 121);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(26, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(94, 121);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(26, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Начало и конец вектора";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "X1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Y1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Z1";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "X2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Y2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(73, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Z2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 76);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(220, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(324, 37);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Фильтрация по модулю вектора(больше указанного значения)  ";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(299, 34);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(74, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Сортировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(301, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(162, 108);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(281, 179);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Группировка по X";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(299, 201);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(162, 108);
+            this.richTextBox2.TabIndex = 19;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(299, 336);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(162, 108);
+            this.richTextBox3.TabIndex = 20;
+            this.richTextBox3.Text = "";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(190, 315);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(192, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Сортировка по возрастанию модуля";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(385, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Сортировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(385, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 23);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Группировать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 406);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 38);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Очистить все поля";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(160, 406);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(120, 38);
+            this.button6.TabIndex = 25;
+            this.button6.Text = "Очистить список векторов";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 455);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ lab7Crossplatform/lab7Crossplatform/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7Crossplatform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool checker = true, checklist = false;
+        double x1, x2, y1, y2, z1, z2, modul;
+
+       
+
+        class Vector
+        {
+            public double x1, x2, y1, y2, z1, z2;
+            public double x, y, z;
+            public double modulvec;
+            public string xyz;
+
+            public Vector(double X1, double X2, double Y1, double Y2, double Z1, double Z2)
+            {
+                x1 = X1;
+                x2 = X2;
+                y1 = Y1;
+                y2 = Y2;
+                z1 = Z1;
+                z2 = Z2;
+
+                x = x2 - x1;
+                y = y2 - y1;
+                z = z2 - z1;
+
+                modulvec = Math.Round(Math.Sqrt(x * x + y * y + z * z), 7);
+
+                xyz = "(" + x + ";" + y + ";" + z + ")";
+
+            }
+        }
+        List<Vector> myvector = new List<Vector>();
+
+
+        async void choosemain()
+        {
+            while (checker)
+            {
+                try
+                {
+                    x1 = Double.Parse(textBox1.Text);
+                    x2 = Double.Parse(textBox2.Text);
+                    y1 = Double.Parse(textBox3.Text);
+                    y2 = Double.Parse(textBox4.Text);
+                    z1 = Double.Parse(textBox5.Text);
+                    z2 = Double.Parse(textBox6.Text);
+                    button1.Enabled = true;
+                }
+                catch
+                {
+                    button1.Enabled = false;
+                }
+                await Task.Delay(100);
+            }
+        }
+        async void choosesecond()
+        {
+            while (checker)
+            {
+                try
+                {
+                    if (checklist)
+                    {
+                       
+                        button4.Enabled = true;
+                        button3.Enabled = true;
+                    }
+                    modul = Double.Parse(textBox7.Text);
+                    if (checklist)
+                    {
+                        button2.Enabled = true;
+                    }
+                }
+                catch
+                {
+                    button2.Enabled = false;
+                }
+                await Task.Delay(100);
+            }
+        }
+
+private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            textBox4.Text = null;
+            textBox5.Text = null;
+            textBox6.Text = null;
+            textBox7.Text = null;
+            richTextBox1.Text = null;
+            richTextBox2.Text = null;
+            richTextBox3.Text = null;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            myvector.Clear();
+            checklist = false;
+            button4.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            choosemain();
+            choosesecond();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myvector.Add(new Vector(x1, x2, y1, y2, z1, z2));
+            checklist = true;
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            textBox4.Text = null;
+            textBox5.Text = null;
+            textBox6.Text = null;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = null;
+            var selectedVector = from vec in myvector
+                                 where vec.modulvec > modul
+                                 select vec;
+            if (selectedVector != null)
+            {
+                int col = 1;
+                foreach (Vector v in selectedVector)
+                {
+                    richTextBox1.Text = richTextBox1.Text + Convert.ToString(col) + " : "+ v.xyz + " | his modul =  " + v.modulvec + Environment.NewLine;
+                    col++;
+                }
+            }
+            else
+            {
+                richTextBox1.Text = "Not Found";
+            }
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = null;
+            var vectors = from vec in myvector
+                            group vec by vec.x;
+
+            foreach (var vec in vectors)
+            {
+                richTextBox2.Text = richTextBox2.Text + " X: " + vec.Key + ", has - " + Environment.NewLine + Environment.NewLine;
+
+                int col = 1;
+
+                foreach (var valuevec in vec)
+                {
+                    richTextBox2.Text = richTextBox2.Text + Convert.ToString(col) + " : " + valuevec.xyz + Environment.NewLine;
+                    col++;
+                }
+                richTextBox2.Text = richTextBox2.Text + Environment.NewLine + Environment.NewLine;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox3.Text = null;
+            var orderedVectors = from v in myvector
+                                 orderby v.modulvec ascending
+                                 select v;
+            int col = 1;
+            foreach(var vec in orderedVectors)
+            {
+                richTextBox3.Text = richTextBox3.Text + Convert.ToString(col) + " : " + vec.xyz + ", modul = " + vec.modulvec + Environment.NewLine;
+                col++;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
