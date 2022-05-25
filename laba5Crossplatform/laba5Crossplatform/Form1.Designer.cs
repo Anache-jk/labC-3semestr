7fc0a86ef5e5ee97d7808c708fc2f2da895a7fac
Anache-jk <83112485+Anache-jk@users.noreply.github.com>
2022-05-25 16:15:56 +0300

    Add files via upload

---
+++ laba5Crossplatform/laba5Crossplatform/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace laba5Crossplatform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 69);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Когда вы проведёте Z квадрат загорится зелёным и сможете попробовать ещё раз, есл" +
+    "и отклонитесь от курса, то квадрат будет красным и также будет доступна кнопка с" +
+    "броса ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 315);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "y";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "x";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "y";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSalmon;
+            this.button1.Location = new System.Drawing.Point(349, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 47);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Сброс";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 355);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ laba5Crossplatform/laba5Crossplatform/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace laba5Crossplatform
+{
+    public partial class Form1 : Form
+    {
+        public int counts;
+        public bool checkerZed = true;
+        public delegate void MoveZtrack();
+        class Point2d
+        {
+            public double x, y;
+          
+            public Point2d(double x, double y)
+            {
+                this.x = x; this.y = y;
+            }
+        }
+        class Zed : Panel
+        {
+            List<Point2d> points = new List<Point2d>();
+            public event MoveZtrack onZed = null;
+            public void CatchZed()
+            {
+                onZed.Invoke();
+            }
+            public void clearpoints()
+            {
+                points.Clear();
+            }
+            public void checkerZ(double x, double y)
+            {
+                points.Add(new Point2d(x, y));
+            }
+            public int checkerZ()
+            {
+                int check = 0, coord1 = 0, coord2 = 0,coord3 = 0,coord4 = 0;
+                bool checkfirstcoord = false;
+                double maxdx = 0, maxhx = 0, mindx = 10000, minhx = 10000;
+                for (int i = 0; i < points.Count; i++)
+                {
+                    if (points[i].x > maxdx)
+                    {
+                        maxdx = points[i].x;
+                        coord1 = i;
+                    }
+                    if (points[i].x < mindx)
+                    {
+                        mindx = points[i].x;
+                        coord3 = i;
+                    }
+                }
+                for (int i = 0; i < points.Count; i++)
+                {
+                    if (points[i].x > maxhx && (points[coord1].y > points[i].y || points[coord1].y < points[i].y))
+                    {
+                        maxhx = points[i].x;
+                        coord2 = i;
+                    }
+                    if (points[i].x < minhx && (points[coord3].y > points[i].y || points[coord3].y < points[i].y))
+                    {
+                        minhx = points[i].x;
+                        coord4 = i;
+                    }
+
+                }                
+                if(points[coord1].y > points[coord2].y)
+                {
+                    int dop = 0;
+                    dop = coord1;
+                    coord1 = coord2;
+                    coord2 = dop;
+                }
+                if(points[coord3].y > points[coord4].y)
+                {
+                    int dop = 0;
+                    dop = coord3;
+                    coord3 = coord4;
+                    coord4 = dop;
+                }
+                for (int i = 0; i < points.Count; i++)
+                {
+                    if((((points[coord2].x-points[coord4].x)/2  - 50 >= points[i].x) ||((points[coord2].x - points[coord4].x) / 2 + 50 <= points[i].x))
+                        && (points[coord2].y > points[i].y) 
+                        && (points[coord1].y < points[i].y)
+                        && (points[coord3].y != points[coord4].y)
+                        && (points[coord1].y != points[coord2].y))
+                    {
+                        check++;
+                     }
+                    if(check >= 10)
+                    {
+                        checkfirstcoord = true;
+                    }
+                }
+           
+                if (checkfirstcoord)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+        }
+      public static void Sub1()
+      {
+            MessageBox.Show("Вы провели по траектории Z, ура?");
+      }
+        
+
+        Zed zobj = new Zed();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            zobj.onZed += Sub1;
+            zobj.Location = new Point(100, 50);
+            zobj.Name = "Bruh";
+            zobj.Size = new Size(200, 200);
+            zobj.BackColor = Color.Black;
+            Controls.Add(zobj);
+        }
+        async void choose()
+        {
+            while (checkerZed)
+            {
+                label2.Text = Convert.ToString(Control.MousePosition.X);
+                label3.Text = Convert.ToString(Control.MousePosition.Y);
+                zobj.checkerZ(Control.MousePosition.X, Control.MousePosition.Y);
+                counts++;
+                if (counts % 200 == 0)
+                {
+                    int result = zobj.checkerZ();
+                    if(result != 1)
+                    {
+                       // MessageBox.Show(Convert.ToString(result));
+                        checkerZed = false;
+                        button1.Enabled = true;
+                        zobj.BackColor = Color.Red;
+                    }
+                    else if (result == 1)
+                    {
+                        zobj.CatchZed();
+                        checkerZed = false;
+                        button1.Enabled = true;
+                        zobj.BackColor = Color.Green;
+                    }
+                    
+                    counts = 0; 
+                    
+                }
+                await Task.Delay(10);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            Thread.Sleep(2000);
+            zobj.clearpoints();
+            counts = 0;
+            checkerZed = true;
+            zobj.BackColor = Color.Black;
+            choose();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
